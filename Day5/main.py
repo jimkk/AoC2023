15f3ad7eb0a00a52309a0910464b11fd115b8d19
Jim <jim-m-kemper@hotmail.com>
2023-12-28 15:19:23 -0700

    day 5 part 2

--- Day5/main.py
+++ Day5/main.py
@@ -13,3 +13,48 @@ for step in steps:
                 numbers[i] = num + (group[0] - group[1])
 
 print('Part 1: ', min(numbers))
+
+# Part 2
+
+def create_sets(a,b):
+    # b in a
+    if a[0] < b[1] and a[0] + a[1] > b[1] + b[2]:
+        return [
+            (a[0], b[1] - a[0]),
+            (b[0], b[2]),
+            (b[1] + b[2], (a[0] + a[1]) - (b[1] + b[2]))
+        ]
+    # a in b
+    elif a[0] > b[1] and a[0] + a[1] < b[1] + b[2]:
+        return [((b[0] - b[1] + a[0]), a[1] )]
+    # lower overlap
+    elif a[0] > b[1] and a[0] < b[1] + b[2]:
+        return [
+                (a[0] + (b[0] - b[1]), (b[1] + b[2]) - a[0]),
+                (b[1] + b[2], a[0] + a[1] - 1)
+            ]
+    # higher overlap
+    elif b[1] < a[0] + a[1] and a[0] + a[1] < b[1] + b[2]:
+        return [
+                (a[0], b[1] - a[0]),
+                (b[0], a[0] + a[1] - b[1])
+            ]
+    # no overlap
+    else:
+        return [a]
+    
+numbers = [int(x) for x in in_data[0].split(':')[1].strip().split(' ')]
+number_sets = [(numbers[x], numbers[x+1]) for x, _ in enumerate(numbers) if x % 2 == 0]
+
+for step in steps:
+    for s in step:
+        for i, n in enumerate(number_sets):
+            new_set = create_sets(n, s)
+            number_sets = sum([number_sets[0:i], new_set, number_sets[i+1:]], [])
+            if len(new_set) > 1:
+                break
+
+x = [x[0] for x in number_sets if x[0] > 0]
+x = sorted(x)
+
+print("Part 2: ", x[0])
